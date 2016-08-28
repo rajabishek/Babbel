f636804ec317e1cec1e2b7cf851d7732cf9ea43b
Raj Abishek <rajabishek@hotmail.com>
2016-08-28 16:15:24 +0530

    Refactor the constraints code: split to individual functions

--- Source/Babbel/RegistrationController.swift
+++ Source/Babbel/RegistrationController.swift
@@ -31,35 +31,73 @@ class RegistrationController: UIViewController {
         return button
     }()
     
+    let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .System)
+        button.backgroundColor = .clearColor()
+        button.setTitle("Forgot Password?", forState: .Normal)
+        button.setTitleColor(UIColor.darkGrayColor(), forState: .Normal)
+        button.titleLabel?.font = UIFont(name: "FiraSans-Regular", size: 12) ?? UIFont.boldSystemFontOfSize(12)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.sizeToFit()
+        return button
+    }()
     
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .Default
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.whiteColor()
-        
         setupLayout()
     }
     
     func setupLayout() {
         view.addSubview(registrationInputsContainer)
         view.addSubview(registerButton)
+        view.addSubview(forgotPasswordButton)
         
         var allConstraints = [NSLayoutConstraint]()
         
-        allConstraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-25-[v0]-25-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
-        allConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(105)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
-        allConstraints.append(NSLayoutConstraint(item: registrationInputsContainer, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: 0))
-        
-        allConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(35)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registerButton])
-        allConstraints.append(NSLayoutConstraint(item: registerButton, attribute: .Width, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .Width, multiplier: 1, constant: 0))
-        allConstraints.append(NSLayoutConstraint(item: registerButton, attribute: .Top, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .Bottom, multiplier: 1, constant: 20))
-        allConstraints.append(NSLayoutConstraint(item: registerButton, attribute: .CenterX, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .CenterX, multiplier: 1, constant: 0))
+        allConstraints += getConstraintsForRegistrationInputsContainer()
+        allConstraints += getConstraintsForRegistrationButton()
+        allConstraints += getConstraintsForForgotPasswordButton()
         
         NSLayoutConstraint.activateConstraints(allConstraints)
     }
     
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return .Default
+    func getConstraintsForRegistrationInputsContainer() -> [NSLayoutConstraint]{
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-25-[v0]-25-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(105)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
+        
+        constraints.append(NSLayoutConstraint(item: registrationInputsContainer, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: 0))
+        
+        return constraints
+    }
+    
+    func getConstraintsForRegistrationButton() -> [NSLayoutConstraint]{
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(35)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registerButton])
+        constraints.append(NSLayoutConstraint(item: registerButton, attribute: .Width, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .Width, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: registerButton, attribute: .Top, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .Bottom, multiplier: 1, constant: 20))
+        constraints.append(NSLayoutConstraint(item: registerButton, attribute: .CenterX, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .CenterX, multiplier: 1, constant: 0))
+        
+        return constraints
+    }
+    
+    func getConstraintsForForgotPasswordButton() -> [NSLayoutConstraint]{
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(NSLayoutConstraint(item: forgotPasswordButton, attribute: .CenterX, relatedBy: .Equal, toItem: registerButton, attribute: .CenterX, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: forgotPasswordButton, attribute: .Top, relatedBy: .Equal, toItem: registerButton, attribute: .Bottom, multiplier: 1, constant: 10))
+        
+        return constraints
     }
 }
