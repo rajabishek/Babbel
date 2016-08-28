43ec01697a1fc5cd8daf6f633ac0592f8d99b236
Raj Abishek <rajabishek@hotmail.com>
2016-08-28 15:13:42 +0530

    Adds the fonts and setup the navigation bar correctly

--- Source/Babbel/HomeController.swift
+++ Source/Babbel/HomeController.swift
@@ -12,6 +12,28 @@ class HomeController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationBar()
+    }
+    
+    func getTitleViewForNavigationBar(title: String, color: UIColor = .whiteColor(), fontName: String = "FiraSans-Regular", fontSize: CGFloat = 17) -> UILabel {
+        
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.font = UIFont(name: fontName, size: fontSize) ?? UIFont.systemFontOfSize(fontSize)
+        titleLabel.numberOfLines = 1
+        titleLabel.textAlignment = .Center
+        titleLabel.textColor = color
+        titleLabel.textAlignment = .Center
+        titleLabel.backgroundColor = .clearColor()
+        titleLabel.sizeToFit()
+        
+        return titleLabel
+    }
+    
+    func setupNavigationBar() {
+        
+        navigationItem.titleView = getTitleViewForNavigationBar("Raj Abishek")
     }
 }
 
