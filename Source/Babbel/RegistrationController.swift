3f54b42ffd82664d90a4380f7b53b1949a680e71
Raj Abishek <rajabishek@hotmail.com>
2016-08-28 17:52:13 +0530

    Toggle the section between registration and login in main screen

--- Source/Babbel/RegistrationController.swift
+++ Source/Babbel/RegistrationController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class RegistrationController: UIViewController {
-
+    
     let registrationInputsContainer: UIView = {
         let container = UIView()
         container.backgroundColor = Color.wildSand
@@ -31,6 +31,27 @@ class RegistrationController: UIViewController {
         return button
     }()
     
+    let loginInputsContainer: UIView = {
+        let container = UIView()
+        container.backgroundColor = Color.wildSand
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.layer.cornerRadius = 4
+        container.layer.masksToBounds = true
+        return container
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .System)
+        button.backgroundColor = Color.baseColor
+        button.setTitle("Login", forState: .Normal)
+        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button.titleLabel?.font = UIFont(name: "FiraSans-Medium", size: 13) ?? UIFont.boldSystemFontOfSize(13)
+        button.layer.cornerRadius = 4
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     let forgotPasswordButton: UIButton = {
         let button = UIButton(type: .System)
         button.backgroundColor = .clearColor()
@@ -42,7 +63,7 @@ class RegistrationController: UIViewController {
         return button
     }()
     
-    let registerLabel: UIButton = {
+    lazy var registerLabel: UIButton = {
         let button = UIButton(type: .System)
         button.backgroundColor = .clearColor()
         button.setTitle("Register", forState: .Normal)
@@ -50,17 +71,18 @@ class RegistrationController: UIViewController {
         button.titleLabel?.font = UIFont(name: "FiraSans-Regular", size: 13) ?? UIFont.boldSystemFontOfSize(13)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.sizeToFit()
+        button.addTarget(self, action: #selector(registerLabelWasClicked), forControlEvents: .TouchUpInside)
         return button
     }()
     
-    let activeSectionBorder: UIView = {
+    let activeSectionHighlight: UIView = {
         let border = UIView()
         border.translatesAutoresizingMaskIntoConstraints = false
         border.backgroundColor = Color.baseColor
         return border
     }()
     
-    let loginLabel: UIButton = {
+    lazy var loginLabel: UIButton = {
         let button = UIButton(type: .System)
         button.backgroundColor = .clearColor()
         button.setTitle("Sign In", forState: .Normal)
@@ -68,8 +90,39 @@ class RegistrationController: UIViewController {
         button.titleLabel?.font = UIFont(name: "FiraSans-Regular", size: 13) ?? UIFont.boldSystemFontOfSize(13)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.sizeToFit()
+        button.addTarget(self, action: #selector(loginLabelWasClicked), forControlEvents: .TouchUpInside)
         return button
     }()
+    
+    let registerSectionContainer: UIView = {
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+    
+    let loginSectionContainer: UIView = {
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+    
+    func loginLabelWasClicked() {
+        activeSectionHighLightLeftConstraint?.constant = registerLabel.frame.width + 20
+        UIView.animateWithDuration(0.35, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .CurveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        self.loginSectionContainer.hidden = false
+        self.registerSectionContainer.hidden = true
+    }
+    
+    func registerLabelWasClicked() {
+        activeSectionHighLightLeftConstraint?.constant = 0
+        UIView.animateWithDuration(0.35, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .CurveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        self.loginSectionContainer.hidden = true
+        self.registerSectionContainer.hidden = false
+    }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .Default
@@ -82,24 +135,40 @@ class RegistrationController: UIViewController {
         setupLayout()
     }
     
+    var activeSectionHighLightLeftConstraint: NSLayoutConstraint?
+    
     func setupLayout() {
-        view.addSubview(registrationInputsContainer)
-        view.addSubview(registerButton)
-        view.addSubview(forgotPasswordButton)
+        
         view.addSubview(registerLabel)
-        view.addSubview(activeSectionBorder)
+        view.addSubview(activeSectionHighlight)
         view.addSubview(loginLabel)
+        
+        view.addSubview(registerSectionContainer)
+        registerSectionContainer.addSubview(registrationInputsContainer)
+        registerSectionContainer.addSubview(registerButton)
+        
+        view.addSubview(loginSectionContainer)
+        loginSectionContainer.addSubview(loginInputsContainer)
+        loginSectionContainer.addSubview(loginButton)
+        loginSectionContainer.addSubview(forgotPasswordButton)
 
         var allConstraints = [NSLayoutConstraint]()
         
+        allConstraints += getConstraintsForRegisterSectionContainer()
         allConstraints += getConstraintsForRegistrationInputsContainer()
         allConstraints += getConstraintsForRegistrationButton()
-        allConstraints += getConstraintsForForgotPasswordButton()
         allConstraints += getConstraintsForRegisterLabel()
-        allConstraints += getConstraintsForRegisterLabelBorder()
+        allConstraints += getConstraintsForActiveSectionHighlight()
         allConstraints += getConstraintsForLoginLabel()
         
+        allConstraints += getConstraintsForLoginSectionContainer()
+        allConstraints += getConstraintsForLoginInputsContainer()
+        allConstraints += getConstraintsForLoginButton()
+        allConstraints += getConstraintsForForgotPasswordButton()
+        
         NSLayoutConstraint.activateConstraints(allConstraints)
+        
+        loginSectionContainer.hidden = true
     }
     
     func getConstraintsForLoginLabel() -> [NSLayoutConstraint] {
@@ -112,15 +181,16 @@ class RegistrationController: UIViewController {
         return constraints
     }
     
-    func getConstraintsForRegisterLabelBorder() -> [NSLayoutConstraint] {
+    func getConstraintsForActiveSectionHighlight() -> [NSLayoutConstraint] {
         
         var constraints = [NSLayoutConstraint]()
         
-        constraints.append(NSLayoutConstraint(item: activeSectionBorder, attribute: .Width, relatedBy: .Equal, toItem: registerLabel, attribute: .Width, multiplier: 1, constant: 0))
-        constraints.append(NSLayoutConstraint(item: activeSectionBorder, attribute: .Top, relatedBy: .Equal, toItem: registerLabel, attribute: .Bottom, multiplier: 1, constant: 0))
-        constraints.append(NSLayoutConstraint(item: activeSectionBorder, attribute: .Left, relatedBy: .Equal, toItem: registerLabel, attribute: .Left, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: activeSectionHighlight, attribute: .Width, relatedBy: .Equal, toItem: registerLabel, attribute: .Width, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: activeSectionHighlight, attribute: .Top, relatedBy: .Equal, toItem: registerLabel, attribute: .Bottom, multiplier: 1, constant: 0))
+        activeSectionHighLightLeftConstraint = NSLayoutConstraint(item: activeSectionHighlight, attribute: .Left, relatedBy: .Equal, toItem: registerLabel, attribute: .Left, multiplier: 1, constant: 0)
+        constraints.append(activeSectionHighLightLeftConstraint!)
         
-        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(2)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": activeSectionBorder])
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(2)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": activeSectionHighlight])
         
         return constraints
     }
@@ -129,8 +199,8 @@ class RegistrationController: UIViewController {
         
         var constraints = [NSLayoutConstraint]()
         
-        constraints.append(NSLayoutConstraint(item: registerLabel, attribute: .Left, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .Left, multiplier: 1, constant: 0))
-        constraints.append(NSLayoutConstraint(item: registerLabel, attribute: .Bottom, relatedBy: .Equal, toItem: registrationInputsContainer, attribute: .Top, multiplier: 1, constant: -20))
+        constraints.append(NSLayoutConstraint(item: registerLabel, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1, constant: 25))
+        constraints.append(NSLayoutConstraint(item: registerLabel, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1, constant: 100))
         
         return constraints
     }
@@ -139,10 +209,22 @@ class RegistrationController: UIViewController {
         
         var constraints = [NSLayoutConstraint]()
         
-        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-25-[v0]-25-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
-        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(105)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(140)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registrationInputsContainer])
         
-        constraints.append(NSLayoutConstraint(item: registrationInputsContainer, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: registrationInputsContainer, attribute: .Top, relatedBy: .Equal, toItem: registerSectionContainer, attribute: .Top, multiplier: 1, constant: 0))
+        
+        return constraints
+    }
+    
+    func getConstraintsForLoginInputsContainer() -> [NSLayoutConstraint]{
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": loginInputsContainer])
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(105)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": loginInputsContainer])
+        
+        constraints.append(NSLayoutConstraint(item: loginInputsContainer, attribute: .Top, relatedBy: .Equal, toItem: loginSectionContainer, attribute: .Top, multiplier: 1, constant: 0))
         
         return constraints
     }
@@ -159,12 +241,49 @@ class RegistrationController: UIViewController {
         return constraints
     }
     
+    func getConstraintsForLoginButton() -> [NSLayoutConstraint]{
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(35)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": loginButton])
+        constraints.append(NSLayoutConstraint(item: loginButton, attribute: .Width, relatedBy: .Equal, toItem: loginInputsContainer, attribute: .Width, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: loginButton, attribute: .Top, relatedBy: .Equal, toItem: loginInputsContainer, attribute: .Bottom, multiplier: 1, constant: 20))
+        constraints.append(NSLayoutConstraint(item: loginButton, attribute: .CenterX, relatedBy: .Equal, toItem: loginInputsContainer, attribute: .CenterX, multiplier: 1, constant: 0))
+        
+        return constraints
+    }
+
+    
     func getConstraintsForForgotPasswordButton() -> [NSLayoutConstraint]{
         
         var constraints = [NSLayoutConstraint]()
         
-        constraints.append(NSLayoutConstraint(item: forgotPasswordButton, attribute: .CenterX, relatedBy: .Equal, toItem: registerButton, attribute: .CenterX, multiplier: 1, constant: 0))
-        constraints.append(NSLayoutConstraint(item: forgotPasswordButton, attribute: .Top, relatedBy: .Equal, toItem: registerButton, attribute: .Bottom, multiplier: 1, constant: 10))
+        constraints.append(NSLayoutConstraint(item: forgotPasswordButton, attribute: .CenterX, relatedBy: .Equal, toItem: loginButton, attribute: .CenterX, multiplier: 1, constant: 0))
+        constraints.append(NSLayoutConstraint(item: forgotPasswordButton, attribute: .Top, relatedBy: .Equal, toItem: loginButton, attribute: .Bottom, multiplier: 1, constant: 10))
+        
+        return constraints
+    }
+    
+    func getConstraintsForLoginSectionContainer() -> [NSLayoutConstraint]{
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-25-[v0]-25-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": loginSectionContainer])
+        let contentsHeight = 105 + 20 + 35 + 10 + forgotPasswordButton.frame.height
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(\(contentsHeight))]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": loginSectionContainer])
+        constraints.append(NSLayoutConstraint(item: loginSectionContainer, attribute: .Top, relatedBy: .Equal, toItem: registerLabel, attribute: .Bottom, multiplier: 1, constant: 20))
+        
+        return constraints
+    }
+    
+    func getConstraintsForRegisterSectionContainer() -> [NSLayoutConstraint]{
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-25-[v0]-25-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registerSectionContainer])
+        let contentsHeight = 140 + 20 + 35
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:[v0(\(contentsHeight))]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": registerSectionContainer])
+        constraints.append(NSLayoutConstraint(item: registerSectionContainer, attribute: .Top, relatedBy: .Equal, toItem: registerLabel, attribute: .Bottom, multiplier: 1, constant: 20))
         
         return constraints
     }
